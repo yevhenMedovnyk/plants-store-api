8751a682c2c74400595fbc5c6ff1b7ed9d1d9b52
Yevhen Medovnyk <yevhenmed@gmail.com>
2025-07-10 11:12:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3006; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
+server.use(cors());
 server.use(router);
 
 server.listen(port);
