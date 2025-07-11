dbf9d559737f17f5857396f11f9462069f5a531a
Yevhen Medovnyk <yevhenmed@gmail.com>
2025-07-11 19:15:32 +0300

    add server options

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3006; // you can use any port number here; i chose to use 3001
-const cors = require("cors");
+const port = process.env.PORT || 3006;
 
 server.use(cors());
+server.options('*', cors()); // 👈 важливо для preflight
 
 server.use(middlewares);
-server.use(cors());
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
